1accf956ab089f9a646ded66251fe19e13b12aae
Denys Savin <dsavin@atlassian.com>
2023-04-09 16:03:52 +0200

    firebase added

--- src/app.ts
+++ src/app.ts
@@ -25,3 +25,5 @@ mongoose
   .catch(error => {
     console.error('Error connecting to MongoDB:', error);
   });
+
+export default app;
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import * as app from './app';
-import * as functions from 'firebase-functions';
-
-export const api = functions.http.onRequest(app);
