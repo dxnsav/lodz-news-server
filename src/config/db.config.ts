52d4ae9371cabf25c613781472f0901b308d96c5
Denys Savin <dsavin@atlassian.com>
2023-04-09 15:43:09 +0200

    fix db linting errors

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -3,12 +3,11 @@ import path from 'path';
 
 dotenv.config({ path: path.resolve(__dirname, '..', '..', '.env'), debug: true });
 
-
 const username = process.env.MONGO_DB_USERNAME;
 const password = process.env.MONGO_DB_PASSWORD;
 const databaseName = process.env.MONGO_DB_NAME;
 
-console.log("username", process.env.MONGO_DB_USERNAME);
+console.log('username', process.env.MONGO_DB_USERNAME);
 
 export const dbConfig = {
   url: `mongodb+srv://${username}:${password}@cluster0.lbip9ob.mongodb.net/${databaseName}?retryWrites=true&w=majority`,
